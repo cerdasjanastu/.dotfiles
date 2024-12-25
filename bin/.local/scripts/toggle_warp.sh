d0f0bea09c0c71fc4b0c696050dae79c10f44e74
cerdasjanastu <cerdasjanastu@gmail.com>
2024-12-25 16:24:56 +0800

    feat: remove unused toggle_warp

--- bin/.local/scripts/toggle_warp.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-STATUS=$(warp-cli status | grep "Connected" | awk '{print $3}')
-
-if [ "$STATUS" == "Connected" ]; then
-    warp-cli disconnect
-else
-    warp-cli connect
-fi
